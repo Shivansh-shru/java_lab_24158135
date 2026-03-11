5a970bcb12eb01ad373d2b1ee8a57c759d8cfd63
shivansh <shankarshivansh28@gmail.com>
2026-03-11 13:37:43 +0530

    ERRORS FIXED

--- GUI2/Day11Question1.java
+++ GUI2/Day11Question1.java
@@ -4,78 +4,88 @@ import java.awt.event.*;
 public class Day11Question1 extends Frame implements ActionListener {
 
     TextField display;
-    Button b[] = new Button[17];
-
-    String s1="", s2="", op="";
+    Button b[];
+    String s1="", op="";
 
     Day11Question1() {
 
-        setTitle("Calculator");
-        setSize(250,300);
-        setLayout(new BorderLayout());
+        setTitle("CALCULATOR");
+        setSize(260,260);
+        setLayout(null);
+        setBackground(Color.cyan);
+
+        Label title = new Label("CALCULATOR");
+        title.setBounds(20,40,100,20);
+        title.setForeground(Color.blue);
+        add(title);
 
         display = new TextField();
-        display.setEditable(false);
-        add(display, BorderLayout.NORTH);
+        display.setBounds(120,40,100,25);
+        add(display);
 
-        Panel p = new Panel();
-        p.setLayout(new GridLayout(5,4,5,5));
-
-        String btn[] = {
+        String btn[]={
                 "1","2","3","+",
                 "4","5","6","-",
                 "7","8","9","*",
-                "/","0","%","=",
-                "OFF"
+                "/","0","%","="
         };
 
-        for(int i=0;i<17;i++) {
-            b[i] = new Button(btn[i]);
-            b[i].addActionListener(this);
-            p.add(b[i]);
+        b = new Button[16];
+
+        int x=20,y=70,k=0;
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                b[k]=new Button(btn[k]);
+                b[k].setBounds(x,y,45,30);
+                b[k].addActionListener(this);
+                add(b[k]);
+
+                x+=50;
+                k++;
+            }
+            x=20;
+            y+=35;
         }
 
-        add(p,BorderLayout.CENTER);
+        Button off = new Button("OFF");
+        off.setBounds(80,210,80,30);
+        off.addActionListener(e -> System.exit(0));
+        add(off);
+
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e){
 
         String cmd = e.getActionCommand();
 
-        if(cmd.matches("[0-9]")) {
+        if(cmd.matches("[0-9]"))
             display.setText(display.getText()+cmd);
-        }
 
-        else if(cmd.matches("[+\\-*/%]")) {
+        else if(cmd.matches("[+\\-*/%]")){
             s1 = display.getText();
             op = cmd;
             display.setText("");
         }
 
-        else if(cmd.equals("=")) {
-
-            s2 = display.getText();
+        else if(cmd.equals("=")){
 
             double a = Double.parseDouble(s1);
-            double b = Double.parseDouble(s2);
-            double r = 0;
+            double b = Double.parseDouble(display.getText());
+            double r=0;
 
-            if(op.equals("+")) r = a+b;
-            if(op.equals("-")) r = a-b;
-            if(op.equals("*")) r = a*b;
-            if(op.equals("/")) r = a/b;
-            if(op.equals("%")) r = a%b;
+            if(op.equals("+")) r=a+b;
+            if(op.equals("-")) r=a-b;
+            if(op.equals("*")) r=a*b;
+            if(op.equals("/")) r=a/b;
+            if(op.equals("%")) r=a%b;
 
             display.setText(String.valueOf(r));
         }
-
-        else if(cmd.equals("OFF")) {
-            System.exit(0);
-        }
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]){
         new Day11Question1();
     }
 }
--- GUI2/Day11Question2.java
+++ GUI2/Day11Question2.java
@@ -3,52 +3,90 @@ import java.awt.event.*;
 
 public class Day11Question2 extends Frame implements ActionListener {
 
-    TextField t1,t2;
-    Button caps,small,reverse,words,letters,vowels,reset;
+    TextField t1, t2;
+    Button caps, small, convert, words, letters, reverse, vowels, frequency, begin, reset;
 
-    Day11Question2(){
+    Day11Question2() {
 
         setTitle("My Frame");
-        setSize(700,400);
-        setLayout(new FlowLayout());
+        setSize(900,500);
+        setLayout(null);
+        setBackground(new Color(230,150,150)); // pink background
 
         Label l1 = new Label("Enter the string:");
-        Label l2 = new Label("String in upper case:");
+        l1.setBounds(80,120,120,20);
+        add(l1);
 
-        t1 = new TextField(40);
-        t2 = new TextField(40);
+        t1 = new TextField();
+        t1.setBounds(250,120,300,25);
+        add(t1);
+
+        Label l2 = new Label("String in upper case:");
+        l2.setBounds(80,180,150,20);
+        add(l2);
+
+        t2 = new TextField();
+        t2.setBounds(250,180,300,25);
+        add(t2);
 
         caps = new Button("IN CAPS");
-        small = new Button("IN SMALL");
-        reverse = new Button("REVERSE");
-        words = new Button("WORDS");
-        letters = new Button("LETTERS");
-        vowels = new Button("VOWELS");
-        reset = new Button("RESET");
+        caps.setBounds(50,300,90,25);
 
-        add(l1); add(t1);
-        add(l2); add(t2);
+        small = new Button("IN SMALL");
+        small.setBounds(180,300,90,25);
+
+        convert = new Button("CONVERT CASE");
+        convert.setBounds(330,300,120,25);
+
+        words = new Button("WORDS");
+        words.setBounds(500,300,90,25);
+
+        letters = new Button("LETTERS");
+        letters.setBounds(650,300,90,25);
+
+        reverse = new Button("REVERSE");
+        reverse.setBounds(110,330,90,25);
+
+        vowels = new Button("VOWELS");
+        vowels.setBounds(300,330,90,25);
+
+        frequency = new Button("FREQUENCY");
+        frequency.setBounds(440,330,100,25);
+
+        begin = new Button("BEGIN CAPS");
+        begin.setBounds(600,330,100,25);
+
+        reset = new Button("RESET");
+        reset.setBounds(400,380,100,30);
+        reset.setBackground(Color.blue);
+        reset.setForeground(Color.white);
 
         add(caps);
         add(small);
-        add(reverse);
+        add(convert);
         add(words);
         add(letters);
+        add(reverse);
         add(vowels);
+        add(frequency);
+        add(begin);
         add(reset);
 
         caps.addActionListener(this);
         small.addActionListener(this);
-        reverse.addActionListener(this);
+        convert.addActionListener(this);
         words.addActionListener(this);
         letters.addActionListener(this);
+        reverse.addActionListener(this);
         vowels.addActionListener(this);
+        frequency.addActionListener(this);
+        begin.addActionListener(this);
         reset.addActionListener(this);
 
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
 
         String s = t1.getText();
 
@@ -58,26 +96,44 @@ public class Day11Question2 extends Frame implements ActionListener {
         if(e.getSource()==small)
             t2.setText(s.toLowerCase());
 
-        if(e.getSource()==reverse){
-            StringBuffer sb = new StringBuffer(s);
-            t2.setText(sb.reverse().toString());
+        if(e.getSource()==convert) {
+            String result="";
+            for(char c : s.toCharArray()) {
+                if(Character.isUpperCase(c))
+                    result += Character.toLowerCase(c);
+                else
+                    result += Character.toUpperCase(c);
+            }
+            t2.setText(result);
         }
 
-        if(e.getSource()==words){
-            String w[] = s.split(" ");
-            t2.setText("Words: "+w.length);
-        }
+        if(e.getSource()==reverse)
+            t2.setText(new StringBuffer(s).reverse().toString());
 
-        if(e.getSource()==letters){
-            t2.setText("Letters: "+s.length());
-        }
+        if(e.getSource()==words)
+            t2.setText("Words: " + s.split(" ").length);
+
+        if(e.getSource()==letters)
+            t2.setText("Letters: " + s.length());
 
         if(e.getSource()==vowels){
             int count=0;
-            for(char c: s.toLowerCase().toCharArray()){
+            for(char c : s.toLowerCase().toCharArray())
                 if("aeiou".indexOf(c)!=-1) count++;
-            }
-            t2.setText("Vowels: "+count);
+            t2.setText("Vowels: " + count);
+        }
+
+        if(e.getSource()==frequency){
+            int freq = s.length();
+            t2.setText("Length: " + freq);
+        }
+
+        if(e.getSource()==begin){
+            String[] words = s.split(" ");
+            String result="";
+            for(String w : words)
+                result += Character.toUpperCase(w.charAt(0)) + w.substring(1) + " ";
+            t2.setText(result);
         }
 
         if(e.getSource()==reset){
@@ -86,7 +142,7 @@ public class Day11Question2 extends Frame implements ActionListener {
         }
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         new Day11Question2();
     }
 }
